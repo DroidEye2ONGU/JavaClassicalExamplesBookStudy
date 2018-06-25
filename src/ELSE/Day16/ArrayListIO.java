7fcc7a92b24fe76dd4fbda70af1c69541449288b
DroidEye2ONGU <droideye.ongu@foxmail.com>
2018-06-25 19:31:24 +0800

    2018.06.25-19:31
    Add Day16 Homeworkd

---
+++ src/ELSE/Day16/ArrayListIO.java
@@ -0,0 +1,35 @@
+package ELSE.Day16;
+
+import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
+public class ArrayListIO {
+    public static void main(String[] args) {
+        ArrayList<Student> data = new ArrayList<>();
+
+        data.add(new Student("it001","张曼玉",35,"北京"));
+        data.add(new Student("it002","王祖贤",33,"上海"));
+        data.add(new Student("it003","林青霞",30,"西安"));
+
+        try(
+                BufferedWriter bw = new BufferedWriter(
+                        new OutputStreamWriter(
+                                new FileOutputStream("a.txt")
+                        )
+                )
+                ) {
+
+            for (int i = 0; i < data.size(); i++) {
+                bw.write(data.get(i).toString2());
+                bw.newLine();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
